3e889a9cb01bae2b2b4baaa87a331ad3a8922cbd
Mykola Melnyk <60305241+melnyk-cto@users.noreply.github.com>
2023-02-02 03:36:39 -0800

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
 console.log(222);
+
+<script>
+  console.log(222);
+</script>
+<script>
+  (function () {
+  console.log(33333);
+})()
+</script>
+<script>
+  document.addEventListener('DOMContentLoaded', function () {
+    console.log(4444);
+    
+  })
+</script>
